167ca9f25176886e466a44df101782dfe8916d79
Dave O'Connor <1656866+HammerMeetNail@users.noreply.github.com>
2019-09-30 16:50:15 -0400

    Update python app to work without args

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ def add(num1, num2):
     return num1 + num2
 
 if __name__ == '__main__':
-    num1 = sys.argv[1]
-    num2 = sys.argv[2]
 
-    total = add(int(num1), int(num2))
+    if len(sys.argv) <= 2:
+        total = add(10, 10)
+    else:
+        num1 = sys.argv[1]
+        num2 = sys.argv[2]
+
+        total = add(int(num1), int(num2))
     print(total)
